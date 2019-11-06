422b1fddffbd431550f650ac37cf668eaada3a24
Marco Bartsch <marcobartsch@hellopc.local>
2019-11-05 16:12:18 -0800

    fix error
    Former-commit-id: 99d120b6babb6e2c50e84bf3f25968d38d3aa20e

--- hackerspace/Website/static/js/axios.js
+++ hackerspace/Website/static/js/axios.js
@@ -1,32 +1,34 @@
 function request_html(parameter, replace_id, inner_OR_outer = 'inner') {
-    axios.get("/get/?" + parameter, {
-            cancelToken: new CancelToken(function executor(c) {
-                // An executor function receives a cancel function as a parameter
-                cancel_search = c;
+    if (document.getElementById(replace_id)) {
+        axios.get("/get/?" + parameter, {
+                cancelToken: new CancelToken(function executor(c) {
+                    // An executor function receives a cancel function as a parameter
+                    cancel_search = c;
+                })
             })
-        })
-        .then(function (response) {
-            if (response.data.html) {
-                if (inner_OR_outer == 'inner') {
-                    document.getElementById(replace_id).innerHTML = response.data.html
-                } else if (inner_OR_outer == 'outer') {
-                    document.getElementById(replace_id).outerHTML = response.data.html
-                }
+            .then(function (response) {
+                if (response.data.html) {
+                    if (inner_OR_outer == 'inner') {
+                        document.getElementById(replace_id).innerHTML = response.data.html
+                    } else if (inner_OR_outer == 'outer') {
+                        document.getElementById(replace_id).outerHTML = response.data.html
+                    }
 
-                // call extra functions if loading a new page
-                parameter = parameter.replace('00', '/')
-                page = parameter.split('=')[1]
-                if (page.includes('/')) {
-                    onLoadFunctions(page)
-                    history.pushState({}, response.data.page_name, page);
-                    document.title = response.data.page_name
+                    // call extra functions if loading a new page
+                    parameter = parameter.replace('00', '/')
+                    page = parameter.split('=')[1]
+                    if (page.includes('/')) {
+                        onLoadFunctions(page)
+                        history.pushState({}, response.data.page_name, page);
+                        document.title = response.data.page_name
+                    }
                 }
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .finally(function () {
-            // always executed
-        });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
+    }
 }
