48f1f4f0fabbfa8ae861ddeeebcd013c8cca11cf
Marco Bartsch <contact@marcobartsch.me>
2019-11-18 02:33:29 +0100

    show first name only

--- hackerspace/templatetags/text.py
+++ hackerspace/templatetags/text.py
@@ -51,3 +51,33 @@ def getDefaultImage(name):
         return 'sewing'
     else:
         return 'class'
+
+
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+
+@register.filter
+def findSearches(text):
+    if not text:
+        return text
+
+    text = cleanhtml(text)
+    searches = re.findall(r'\{([^{}]*)\}\|search', text)
+    for search_query in searches:
+        text = text.replace('{'+search_query+'}|search', '<a class="inline_link" onclick="openMenu();enterSearch(\'' +
+                            search_query+'\')" href="#">'+search_query+'</a>')
+
+    return text
+
+
+@register.filter
+def TEXT_replaceLinkCSS(text):
+    return text.replace('class="linkified"', 'target="_blank" class="inline_link"')
+
+
+@register.filter
+def STR__first_name(text):
+    return text.split(' ')[0]
