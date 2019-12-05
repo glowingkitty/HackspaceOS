bdc584b8ad216925d7f61bb50c0e1d877627eecc
Marco Bartsch <contact@marcobartsch.me>
2019-12-05 11:41:25 +0200

    detect if slack key exist

--- hackerspace/APIs/slack.py
+++ hackerspace/APIs/slack.py
@@ -1,18 +1,21 @@
 import os
 import slack
-from getKey import STR__get_key
+from getKey import STR__get_key, BOOLEAN__key_exists
 
 # API documentation: https://github.com/slackapi/python-slackclient
-
-client = slack.WebClient(token=STR__get_key('SLACK.API_TOKEN'))
 
 
 def send_message(message, channel='#general'):
     print('LOG: send_message(message, channel={})'.format(channel))
-    # see https://github.com/slackapi/python-slackclient#sending-a-message-to-slack
-    response = client.chat_postMessage(channel=channel, text=message)
-
-    if response['ok'] == True:
-        print('LOG: --> Success! Sent message to Slack')
+    if BOOLEAN__key_exists('SLACK.API_TOKEN') == False:
+        print('LOG: --> KEY MISSING: SLACK.API_TOKEN not defined. Couldnt sent notification via Slack.')
     else:
-        print('LOG: --> Failed!')
+        client = slack.WebClient(token=STR__get_key('SLACK.API_TOKEN'))
+
+        # see https://github.com/slackapi/python-slackclient#sending-a-message-to-slack
+        response = client.chat_postMessage(channel=channel, text=message)
+
+        if response['ok'] == True:
+            print('LOG: --> Success! Sent message to Slack')
+        else:
+            print('LOG: --> Failed!')
