7fc38d43b9bb35c6dc84a72a7cab4d76f0944249
Marco Bartsch <contact@marcobartsch.me>
2019-12-16 17:15:20 +0200

    unified separate translation files

--- hackerspace/templatetags/translate.py
+++ hackerspace/templatetags/translate.py
@@ -6,93 +6,81 @@ register = Library()
 def landingpage(text, language):
     from django.template.loader import get_template
     try:
-        return get_template('translations/landingpage/'+language+'.html').render({
-            'word': text
+        return get_template('translations/landingpage.html').render({
+            'word': text,
+            'language': language
         })
     except:
-        try:
-            return get_template('translations/landingpage/english.html').render({
-                'word': text
-            })
-        except:
-            return text
+        return text
 
 
 @register.filter
 def donate(text, language):
     from django.template.loader import get_template
     try:
-        return get_template('translations/donate/'+language+'.html').render({
-            'word': text
+        return get_template('translations/donate.html').render({
+            'word': text,
+            'language': language
         })
     except:
-        try:
-            return get_template('translations/donate/english.html').render({
-                'word': text
-            })
-        except:
-            return text
+        return text
 
 
 @register.filter
 def menu(text, language):
     from django.template.loader import get_template
     try:
-        return get_template('translations/menu/'+language+'.html').render({
-            'word': text
+        return get_template('translations/menu.html').render({
+            'word': text,
+            'language': language
         })
     except:
-        try:
-            return get_template('translations/menu/english.html').render({
-                'word': text
-            })
-        except:
-            return text
+        return text
 
 
 @register.filter
 def values(text, language):
     from django.template.loader import get_template
     try:
-        return get_template('translations/values/'+language+'.html').render({
-            'word': text
+        return get_template('translations/values.html').render({
+            'word': text,
+            'language': language
         })
     except:
-        try:
-            return get_template('translations/values/english.html').render({
-                'word': text
-            })
-        except:
-            return text
+        return text
+
+
+@register.filter
+def results_lists(text, language):
+    from django.template.loader import get_template
+    try:
+        return get_template('translations/results_lists.html').render({
+            'word': text,
+            'language': language
+        })
+    except:
+        return text
 
 
 @register.filter
 def events(text, language):
     from django.template.loader import get_template
     try:
-        return get_template('translations/events/'+language+'.html').render({
-            'word': text
+        return get_template('translations/events.html').render({
+            'word': text,
+            'language': language
         })
     except:
-        try:
-            return get_template('translations/events/english.html').render({
-                'word': text
-            })
-        except:
-            return text
+        return text
 
 
 @register.filter
 def photos(text, language):
     from django.template.loader import get_template
     try:
-        return get_template('translations/photos/'+language+'.html').render({
-            'word': text
+        return get_template('translations/photos.html').render({
+            'word': text,
+            'language': language
         })
     except:
-        try:
-            return get_template('translations/photos/english.html').render({
-                'word': text
-            })
-        except:
-            return text
+        return text
